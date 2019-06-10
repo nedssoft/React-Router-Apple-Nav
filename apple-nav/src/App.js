cbda08656916cad6e75bda54c53fc768c92bc03e
Orie Chinedu <oriechinedu@gmail.com>
2019-06-10 14:17:30 +0100

    Setup project

---
+++ apple-nav/src/App.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import GlobalStyle from './theme/GlobalStyle'
+
+import './App.css';
+
+function App() {
+  return (
+    <React.Fragment>
+      <GlobalStyle />
+      <div className="App">
+        
+      </div>
+    </React.Fragment>
+  );
+}
+
+export default App;
